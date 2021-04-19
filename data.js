545eca754303024f60e9e0ac3ebc04c9178a7e42
OnlyGideon <81019320+Onlygideon@users.noreply.github.com>
2021-04-19 08:46:47 -0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,20 @@
+const axios = require('axios')
+const fs = require('fs')
+
+
+const fetchData = async() =>{
+    try{
+        const url = 'http://jsonplaceholder.typicode.com/posts'
+        const res = await axios.get(url)
+        const result = JSON.stringify(res.data, null, 2)
+        fs.writeFile('./result/posts.json', result, (err) =>{
+            if (err) throw err
+            console.log('File Written Successfully')
+        })
+    } catch (error){
+        console.log(error)
+    }
+}
+
+fetchData()
+
